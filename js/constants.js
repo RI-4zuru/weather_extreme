6af587e6ab0c810ced3b6de8edf60e283b524ada
RI <2391key5145@gmail.com>
2026-04-27 18:02:25 +0900

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -62,13 +62,13 @@ export const LIVE_SUPPORTED_ANNUAL_KEYS = new Set([
 
 export const LIVE_SUPPORTED_MONTHLY_KEYS = new Set([
   "monthMax3hPrecip",
-  //"monthMax6hPrecip",
-  //"monthMax12hPrecip",
+  "monthMax6hPrecip",
+  "monthMax12hPrecip",
   "monthMax24hPrecip",
-  //"monthMax48hPrecip",
-  //"monthMax72hPrecip",
-  //"monthDeepSnowHigh",
-  //"monthDeepSnowLow",
+  "monthMax48hPrecip",
+  "monthMax72hPrecip",
+  "monthDeepSnowHigh",
+  "monthDeepSnowLow",
   "monthMax6hSnow",
   "monthMax24hSnow",
 ]);
